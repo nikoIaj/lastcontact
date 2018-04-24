384c93e445a35c1cd3245c7c28ae2d88ccac744f
Mission Bit <missionbit@green-dl.local>
2018-04-24 16:29:41 -0700

    all levels working

---
+++ js/level5.js
@@ -0,0 +1,19 @@
+/**
+ * File: level2.js
+ *
+ * This is the first level state
+ */
+
+const level5 = {
+  preload: function () {
+    globalPreload();
+  },
+
+  create: function () {
+    globalCreate();
+  },
+
+  update: function() {
+    globalUpdate();
+  }
+};
